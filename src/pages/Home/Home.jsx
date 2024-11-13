82ed6af59cdfd8f3266287a205342274eab1d5de
+ <kibetg984@gmail.com>
2024-11-13 10:22:02 +0300

    Added our list of coins

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,44 +1,47 @@
-import React, { useContext, useEffect, useState} from 'react'
-import './Home.css'
-import { CoinContext } from '../../context/CoinContext'
+import React, { useContext, useEffect, useState } from 'react';
+import './Home.css';
+import { CoinContext } from '../../context/CoinContext';
 
 const Home = () => {
-
-  const {allCoin, currency} = useContext(CoinContext);
+  const { allCoin } = useContext(CoinContext); // Removed 'currency' since it's unused
   const [displayCoin, setDisplayCoin] = useState([]);
   const [input, setInput] = useState('');
 
-  const inputHandler = (event)=>{
+  const inputHandler = (event) => {
     setInput(event.target.value);
-    if(event.target.value === ""){
+    if (event.target.value === "") {
       setDisplayCoin(allCoin);
     }
+  };
 
-  }
-
-  const searchHandler = async (event)=>{
+  const searchHandler = async (event) => {
     event.preventDefault();
-     const coins = await allCoin.filter((item)=>{
-       return item.name.toLowerCase().includes(input.toLowerCase())
-    })
+    const coins = await allCoin.filter((item) => {
+      return item.name.toLowerCase().includes(input.toLowerCase());
+    });
     setDisplayCoin(coins);
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     setDisplayCoin(allCoin);
-  },[allCoin])
+  }, [allCoin]);
 
   return (
     <div className='home'>
       <div className='hero'>
         <h1>Krypto Maniac <br/> The Crypto Marketplace</h1>
         <p>
-          Welcome to krypto maniac, the best place to Check on your Market Prices.
+          Welcome to Krypto Maniac, the best place to check on your market prices.
         </p>
         <form onSubmit={searchHandler}>
-
-
-          <input onChange={inputHandler} list='coinlist' value={input} type="text" placeholder='Search crypto..' required/>
+          <input 
+            onChange={inputHandler} 
+            list='coinlist' 
+            value={input} 
+            type="text" 
+            placeholder='Search crypto..' 
+            required 
+          />
           <button type="submit">Search</button>
         </form>
       </div>
@@ -51,11 +54,19 @@ const Home = () => {
           <p className='market-cap'>Market Cap</p>
         </div>
         {
-         
+          displayCoin.map((coin, index) => (
+            <div className="table-layout" key={coin.id}>
+              <p>{index + 1}</p>
+              <p>{coin.name}</p>
+              <p>{coin.current_price}</p>
+              <p style={{ textAlign: "center" }}>{coin.price_change_percentage_24h}%</p>
+              <p className='market-cap'>{coin.market_cap}</p>
+            </div>
+          ))
         }
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
