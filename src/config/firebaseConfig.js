906b218af03136c0dffe930172371540a61d500e
Kibet-g <kibetg984@gmail.com>
2026-01-18 15:25:29 +0300

    Security Fix: Removed hardcoded API key

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,15 +1,12 @@
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: "crypto-8ddf7",
+  storageBucket: "crypto-8ddf7.appspot.com",
+  messagingSenderId: "635110986095",
+  appId: "1:635110986095:web:c15d60f5f56d9b5026189a",
+  measurementId: "G-J6PE02XD2Y"
+};
 
-
-  const firebaseConfig = {
-    apiKey: "AIzaSyDdwoIiag-2vS7ljLqiK_Y5yqsRbMZVpmk",
-    authDomain: "crypto-8ddf7.firebaseapp.com",
-    databaseURL: "https://crypto-8ddf7-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "crypto-8ddf7",
-    storageBucket: "crypto-8ddf7.appspot.com",
-    messagingSenderId: "635110986095",
-    appId: "1:635110986095:web:c15d60f5f56d9b5026189a",
-    measurementId: "G-J6PE02XD2Y"
-  };
-
-
-  export default firebaseConfig;
+export default firebaseConfig;
